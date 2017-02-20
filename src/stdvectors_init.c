4c7589373d4b9fb3e9b62df2a454109722dfd1a9
digEmAll <mgiuliano.mail@gmail.com>
2017-02-20 22:19:21 +0100

    Increase version to 0.0.5

--- src/stdvectors_init.c
+++ src/stdvectors_init.c
@@ -3,10 +3,6 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
 /* .Call calls */
 extern SEXP stdvectors_getStringFromType(SEXP);
 extern SEXP stdvectors_getTypeFromString(SEXP);
